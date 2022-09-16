daeed5c698a9e918306bcefd4937423f3f2d50e8
Dario Djuric <dario@thisdot.co>
2022-09-16 17:56:52 +0200

    fix: wrong route

--- islands/ConverterForm.tsx
+++ islands/ConverterForm.tsx
@@ -8,7 +8,7 @@ interface ConverterFormProps {
 
 export default function ConverterForm({ currencies = [] }: ConverterFormProps) {
   return (
-    <form className={tw`bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4`} action="/routes/convert">
+    <form className={tw`bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4`} action="/convert">
       <div className={tw`flex`}>
         <div className={tw`mb-4 px-1`}>
           <label className={tw`block text-gray-700 text-sm font-bold mb-2`} htmlFor="amount">
