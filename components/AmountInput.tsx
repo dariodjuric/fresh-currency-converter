d7d754c142b20058c8e00e45183f91974254ff45
Dario Djuric <dario@thisdot.co>
2022-09-24 19:19:11 +0200

    refactor: ConverterForm to multiple components

---
+++ components/AmountInput.tsx
@@ -0,0 +1,16 @@
+interface AmountInputProps {
+  value?: number;
+}
+
+export default function AmountInput({ value }: AmountInputProps) {
+  return (
+    <div className="mb-4 px-1">
+      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="amount">
+        Amount
+      </label>
+      <input
+        className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id="amount" name="amount" type="number" placeholder="0.0" value={value || 0} required/>
+    </div>
+  );
+}
--- islands/ConverterForm.tsx
+++ islands/ConverterForm.tsx
@@ -1,5 +1,7 @@
 import supportedCurrencies from '../supported-currencies.json' assert { type: 'json' };
 import { useState } from 'https://esm.sh/stable/preact@10.11.0/deno/hooks.js';
+import AmountInput from '../components/AmountInput.tsx';
+import CurrencySelect from '../components/CurrencySelect.tsx';
 
 const currencyCodes = Object.entries(supportedCurrencies.fiats).map(([, currency]) => currency.currency_code);
 
@@ -15,41 +17,14 @@ export default function ConverterForm({amount, from, to}: ConverterFormProps) {
   return (
     <form className="" action="/convert" onSubmit={() => setSubmitted(true)}>
       <div className="flex">
-        <div className="mb-4 px-1">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="amount">
-            Amount
-          </label>
-          <input
-            className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="amount" name="amount" type="number" placeholder="0.0" value={amount || 0} required/>
-        </div>
-        <div className="mb-6 px-1">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="from">
-            From
-          </label>
-          <select
-            className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="from"
-            name="from">
-            {currencyCodes.map((currency) => <option key={currency} selected={from ? from === currency : currency === 'USD'} value={currency}>{currency}</option>)}
-          </select>
-        </div>
-        <div className="mb-6 px-1">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="to">
-            To
-          </label>
-          <select
-            className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="to"
-            name="to">
-            {currencyCodes.map((currency) => <option key={currency} selected={to ? to === currency : currency === 'EUR'} value={currency}>{currency}</option>)}
-          </select>
-        </div>
+        <AmountInput value={amount}></AmountInput>
+        <CurrencySelect currencies={currencyCodes} defaultCurrency="USD" name="from" selectedCurrency={from} label="From"></CurrencySelect>
+        <CurrencySelect currencies={currencyCodes} defaultCurrency="EUR" name="to" selectedCurrency={to} label="To"></CurrencySelect>
       </div>
       <div className="flex items-center justify-center px-2">
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="submit">
+          type="submit" disabled={submitted}>
           {submitted ? 'Converting...' : 'Convert'}
         </button>
       </div>
